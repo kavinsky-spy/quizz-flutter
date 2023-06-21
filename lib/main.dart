4811db7ff5a9cf14f45ebf0be7bc4226a3187db7
kavinsky-spy <juliorapa@hotmail.com>
2023-06-21 20:26:15 -0300

    split question into a different class

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quizz_flutter/question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -27,19 +28,33 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Widget> scoreKeeper = [];
 
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.'
-  ];
-
   int questionNumber = 0;
 
-  void nextQuestion() {
+  void nextQuestion(bool answer) {
+    bool correctAnswer = questionsList[questionNumber].questionAnswer;
+
+    if (correctAnswer == answer) {
+      setState(() {
+        questionNumber++;
+      });
+      return;
+    } else {}
     setState(() {
       questionNumber++;
     });
+    return;
   }
+
+  List<Question> questionsList = [
+    Question(q: 'You can lead a cow down stairs but not up stairs', a: false),
+    Question(
+        q: 'Approximately one quarter of human bones are in the feet.',
+        a: true),
+    Question(q: 'A slug\'s blood is green.', a: true)
+  ];
+
+  Question q1 =
+      Question(q: 'You can lead a cow down stairs but not up stairs', a: false);
 
   @override
   Widget build(BuildContext context) {
@@ -50,12 +65,12 @@ class _QuizPageState extends State<QuizPage> {
         Expanded(
           flex: 5,
           child: Padding(
-            padding: EdgeInsets.all(10.0),
+            padding: const EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                questionsList[questionNumber].questionText,
                 textAlign: TextAlign.center,
-                style: TextStyle(
+                style: const TextStyle(
                   fontSize: 25.0,
                   color: Colors.white,
                 ),
@@ -77,7 +92,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                nextQuestion();
+                nextQuestion(true);
                 //The user picked true.
               },
             ),
@@ -97,7 +112,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                nextQuestion();
+                nextQuestion(false);
                 //The user picked false.
               },
             ),
